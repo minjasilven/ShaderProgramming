bf0ec84a459c6d5dcbfebdc7a77b8bdebe672f89
Tuiaa <minjasilvennoinen@gmail.com>
2018-06-26 21:55:49 +0300

    Started simple water shader

---
+++ Assets/Shaders/Tutorials/Simple Water Shader/DepthTexture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DepthTexture : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+        _camera.depthTextureMode = DepthTextureMode.Depth;
+    }
+}
